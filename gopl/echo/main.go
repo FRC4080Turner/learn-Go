e099caf3fdb9298403b990b6fae0181c991b23cc
Joshua Turner <joshflyer5tp@gmail.com>
2017-12-24 14:53:08 -0500

    args print on newlines with indicators

--- gopl/echo/main.go
+++ gopl/echo/main.go
@@ -5,11 +5,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	// Assign multiple variables as strings
-	var arg, separator string
+	var arg string
+	separator := "\n"
 
 	/*
 		Set i to one, and as long as it is
@@ -17,8 +19,9 @@ func main() {
 		keep running.
 	*/
 	for i := 1; i < len(os.Args); i++ {
-		arg += separator + os.Args[i]
-		separator = " "
+		// Arguments on separate line w/indicators
+		arg += "Argument #" + strconv.Itoa(i) + ": " + os.Args[i] + separator
+
 	}
 	fmt.Println(arg)
 }
