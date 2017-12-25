500b13f700c475eef1731ed8b4255cbcd4585c17
Joshua Turner <joshflyer5tp@gmail.com>
2017-12-25 00:28:14 -0500

    more comments

--- gopl/dup3/main.go
+++ gopl/dup3/main.go
@@ -12,13 +12,13 @@ import (
 func main() {
 	counts := make(map[string]int) // make a map with strings as keys that link int values
 	for _, filename := range os.Args[1:] {
-		data, err := ioutil.ReadFile(filename)
+		data, err := ioutil.ReadFile(filename) // read the file into the variable "data". ReadFile outputs file as a byte slice
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
 			continue
 		}
-		for _, line := range strings.Split(string(data), "\n") {
-			counts[line]++
+		for _, line := range strings.Split(string(data), "\n") { // split the file into its induvidulal lines
+			counts[line]++ // add text as a key and increment every time the same text shows up
 		}
 	}
 	for line, n := range counts {
