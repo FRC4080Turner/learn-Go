ff7a4301d1cc7289b2c67b8caf8d9235af5104e2
Joshua Turner <joshflyer5tp@gmail.com>
2017-12-27 00:21:54 -0500

    finished var declaration

---
+++ lhtc/03_variables/01_shorthand/01/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a string
+	var b int32
+	var c uint32
+	var d float64
+	var e bool
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+	fmt.Printf("%v \n", e)
+}
