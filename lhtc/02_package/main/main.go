68f2fad92d346680caa949dba7da7e90230ffe72
Joshua Turner <joshflyer5tp@gmail.com>
2017-12-26 23:10:03 -0500

    added files for package lesson and code for main

---
+++ lhtc/02_package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jman4747/learn-go/lhtc/02_package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
